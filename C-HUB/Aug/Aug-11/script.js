44fdf08972084c8ce76422b9ac9d32ca93c58fb2
AbhiNamdev025 <namdevabhi025@gmail.com>
2025-09-04 08:58:57 +0530

    Assignment Added

---
+++ C-HUB/Aug/Aug-11/script.js
@@ -0,0 +1,20 @@
+// Table Calulation
+
+function table(a) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(a * i);
+  }
+}
+
+for (let i = 5; i <= 9; i++) {
+  console.log("Table of :", i);
+  table(i);
+}
+
+// let a = prompt("Enter A Number :");
+// let i = 1;
+// console.log("Table of ", a);
+// while (i <= 10) {
+//   console.log(a * i);
+//   i++;
+// }
